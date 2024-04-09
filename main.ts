f788307bac7e6e267be5bb879b121eb0e8dad905
Stephany Brazeiro <stephanybrazeiro@gmail.com>
2024-04-09 13:12:48 -0300

    exercicios do 1 ao 4

---
+++ main.ts
@@ -0,0 +1,40 @@
+//1
+// for(let i = 0; i < 41; i++){
+//   console.log(i);
+// }
+
+
+// 2
+// function leiaNumero(numero: number){
+//   let resultado = [];
+//   for(let i = 0; i <= numero; i++){
+//    resultado[i] = i;
+//   }
+//   return resultado;
+// }
+
+// console.log(leiaNumero(7));
+
+
+// 3
+// function leiaDoisNumeros(m: number, n:number){
+//   if(m < n){
+//     for(let i = m; i <= n; i++){
+//       console.log(i)
+//     }
+//   }
+// }
+
+// console.log(leiaDoisNumeros(5,10));
+
+// //4 
+// function leiaDoisNumerosImpares(m: number, n: number) {
+//   for (let i = m; i <= n; i++) {
+//     if (i % 2 !== 0) {
+//       console.log(i)
+//     }
+//   }
+// }
+
+// console.log(leiaDoisNumerosImpares(0, 10));
+
