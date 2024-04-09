4cc0b9bc9874cd0b1700e475f6d51a6ca432d2ed
Cecília Barbosa <105225682+barbo347@users.noreply.github.com>
2024-04-09 15:58:07 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -51,3 +51,58 @@
 // }
 
 // console.log(leiaDoisNumerosPares(0, 10));
+
+//6
+// function leiaDoisNumerosDiv3 (m: number, n: number) {
+//   for (let i = m; i <= n; i++) {
+//     if (i % 3 === 0 && i != 0) {
+//       console.log(i);
+
+//     }
+//   }
+// }
+
+// console.log(leiaDoisNumerosDiv3(0, 10));
+
+//7
+// function leiaM (m: number, id: number, n: number) {
+//   for (let i = 0; i < n; i++) {
+//     console.log(m)
+//     m += id;
+//   }
+
+// }
+
+// console.log(leiaM(6, 3, 4));
+
+// 8
+// function MaiorMenor(quant: number, array: number[]) {
+//   let maior = array[0];
+//   let menor = array[0];
+//   for (let i = 0; i < quant; i++) {
+
+//     if (array[i] > maior) {
+//       maior = array[i];
+//     }
+//     else if (array[i] < menor) {
+//       menor = array[i];
+//     }
+//   }
+//   console.log(maior, menor);
+//   return `Maior: ${maior} Menor: ${menor}`;
+// }
+
+
+// console.log(MaiorMenor(8, [9, 2, 7, 8, 3, 6, 2, 1]));
+
+//9
+// function leianumeros (n: number[]){
+//   n.sort((a, b) => b - a);
+//   for (let i = 0; i < 3; i++){
+//     console.log(n[i])
+//   }
+// }
+
+
+
+// console.log(leianumeros([6, 2, 1, 399, 2, 3, 1]))
